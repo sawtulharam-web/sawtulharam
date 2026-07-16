2f1fdd8af53fc9bfe1abd7aa723beea40364fb6a
sawtulharam-web <alsawtulharam@gmail.com>
2026-07-17 01:16:17 +0500

    daily reminder ayahs
    daily ayahs new file created - database of ayahs

---
+++ sawtul-haram-standalone/src/data/dailyAyahs.ts
@@ -0,0 +1,44 @@
+export interface DailyAyahReference {
+  surah: number;
+  ayah: number;
+}
+
+
+export const DAILY_AYAHS: DailyAyahReference[] = [
+  { surah: 1, ayah: 1 },
+  { surah: 2, ayah: 286 },
+  { surah: 3, ayah: 8 },
+  { surah: 3, ayah: 139 },
+  { surah: 13, ayah: 28 },
+  { surah: 14, ayah: 7 },
+  { surah: 17, ayah: 80 },
+  { surah: 18, ayah: 10 },
+  { surah: 20, ayah: 46 },
+  { surah: 21, ayah: 83 },
+  { surah: 21, ayah: 87 },
+  { surah: 24, ayah: 35 },
+  { surah: 25, ayah: 74 },
+  { surah: 27, ayah: 19 },
+  { surah: 28, ayah: 24 },
+  { surah: 29, ayah: 69 },
+  { surah: 33, ayah: 56 },
+  { surah: 39, ayah: 53 },
+  { surah: 40, ayah: 60 },
+  { surah: 41, ayah: 34 },
+  { surah: 42, ayah: 43 },
+  { surah: 47, ayah: 7 },
+  { surah: 55, ayah: 13 },
+  { surah: 59, ayah: 22 },
+  { surah: 65, ayah: 2 },
+  { surah: 65, ayah: 3 },
+  { surah: 67, ayah: 2 },
+  { surah: 73, ayah: 20 },
+  { surah: 89, ayah: 27 },
+  { surah: 93, ayah: 3 },
+  { surah: 93, ayah: 5 },
+  { surah: 94, ayah: 5 },
+  { surah: 94, ayah: 6 },
+  { surah: 103, ayah: 1 },
+  { surah: 103, ayah: 2 },
+  { surah: 103, ayah: 3 }
+];
