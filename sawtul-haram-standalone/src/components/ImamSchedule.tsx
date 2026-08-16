cd0bc6541b99da0c584efe19efee371cd35b6f34
sawtulharam-web <alsawtulharam@gmail.com>
2026-08-17 01:07:03 +0500

    Update ImamSchedule.tsx - Opacity

--- sawtul-haram-standalone/src/components/ImamSchedule.tsx
+++ sawtul-haram-standalone/src/components/ImamSchedule.tsx
@@ -18,7 +18,7 @@ export default function ImamSchedule() {
 
   return (
     <section id="schedule" className="py-24 bg-[#f3efe6] relative overflow-hidden">
-      <IslamicPattern opacity={0.05} className="text-primary" animate={true} />
+      <IslamicPattern opacity={0.13} className="text-primary" animate={true} />
 
       <div className="container mx-auto px-4 md:px-6 relative z-10">
         {/* Heading */}
